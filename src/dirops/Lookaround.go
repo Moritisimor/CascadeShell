2fd74370cd39998b20eca09e8dfb4dd02187ebf2
Moritisimor <devMoritisimor@proton.me>
2025-10-07 10:07:54 +0200

    Fixed bug where Lookaround wouldn't work on directories which are not the current one.

--- src/dirops/Lookaround.go
+++ src/dirops/Lookaround.go
@@ -7,40 +7,36 @@ import (
 )
 
 func Lookaround(dirName string, showhidden bool) {
-	var dir string
-	if strings.TrimSpace(dirName) == "" {
-		dir = "./"
-	}
-
-	dirs, err := os.ReadDir(dir)
+	dirs, err := os.ReadDir("./" + dirName)
 
 	if err != nil {
-		fmt.Printf("Something went while reading the entries of %s. Make sure %s is a folder.\n", dirName, dirName)
-	} else {
-		entryCounter := 0
-		fmt.Println("Subdirectories and Files:")
-		for _, entry := range(dirs) {
-			entryCounter++
-			entryType := ""
-			if entry.Type().IsDir() {
-				entryType = "Folder"
-			} else if entry.Type().IsRegular() {
-				entryType = "File"
-			} else {
-				entryType = "Misc"
-			}
+		fmt.Printf("Something went while reading the entries of %s. Make sure %s is a folder and exists.\n", dirName, dirName)
+		return
+	}
 
-			if !showhidden {
-				if !strings.HasPrefix(entry.Name(), ".") {
-					fmt.Printf("-> %s (%s)\n", entry.Name(), entryType)
-				}
-			} else {
+	entryCounter := 0
+	fmt.Println("Subdirectories and Files:")
+	for _, entry := range(dirs) {
+		entryCounter++
+		entryType := ""
+		if entry.Type().IsDir() {
+			entryType = "Folder"
+		} else if entry.Type().IsRegular() {
+			entryType = "File"
+		} else {
+			entryType = "Misc"
+		}
+
+		if !showhidden {
+			if !strings.HasPrefix(entry.Name(), ".") {
 				fmt.Printf("-> %s (%s)\n", entry.Name(), entryType)
 			}
+		} else {
+			fmt.Printf("-> %s (%s)\n", entry.Name(), entryType)
 		}
+	}
 		
-		if entryCounter == 0 {
-			fmt.Println("Empty directory.")
-		}
+	if entryCounter == 0 {
+		fmt.Println("Empty directory.")
 	}
 }
