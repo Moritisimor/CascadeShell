3bda8c726c478dc01eb6dd28dce85edc95d3dfd6
Moritisimor <devMoritisimor@proton.me>
2025-10-12 15:02:49 +0200

    Changed how the prompt looks

--- src/funcs/smallhelpers/DrawPrompt.go
+++ src/funcs/smallhelpers/DrawPrompt.go
@@ -3,11 +3,15 @@ package smallhelpers
 import "CaSh/funcs/color"
 
 func Drawprompt(user string, host string, currentDir string) {
-	color.PrintBlue("( ")
+	color.PrintBlue("[")
 	color.PrintGreen(user)
 	color.PrintBlue("@")
 	color.PrintMagenta(host)
 	color.PrintBlue("#")
 	color.PrintYellow(currentDir)
-	color.PrintBlue(" ) -> ")
+	if user == "root" {
+		color.PrintBlue("] >>> ")
+	} else {
+		color.PrintBlue("] >> ")
+	}
 }
