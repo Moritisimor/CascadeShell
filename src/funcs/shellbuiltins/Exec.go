7a96850e1272be4d4ed1c370b07aaa8430c3a442
Moritismor <devMoritisimor@proton.me>
2025-10-21 03:00:51 +0200

    Added Command time Duration after execution

--- src/funcs/shellbuiltins/Exec.go
+++ src/funcs/shellbuiltins/Exec.go
@@ -4,22 +4,26 @@ import (
 	"CaSh/funcs/color"
 	"CaSh/funcs/smallhelpers"
 	"fmt"
+	"math"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func Execute(funcArgs []string, processFlag *bool) {
+	startTime := time.Now()
 	cmd := exec.Command(funcArgs[0], funcArgs[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	smallhelpers.SetTrue(processFlag) // Mark process as running
-	runErr:= cmd.Run()
+	runErr := cmd.Run()
 	defer smallhelpers.SetFalse(processFlag) // Mark as done at the end of the scope
 
 	if runErr != nil {
 		color.PrintRedln(runErr.Error())
 	} else {
-		color.PrintGreenln(fmt.Sprintf("\nCommand %s executed successfully.", funcArgs[0]))
+		difference := math.Abs(float64(time.Until(startTime)) / 1000000000)
+		color.PrintGreenln(fmt.Sprintf("\nCommand %s executed successfully in %.2f second/s.", funcArgs[0], difference))
 	}
 }
